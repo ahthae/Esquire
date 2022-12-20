9427b2a6c2f0b6b70fc72eea4ca3db26fdac20c9
ahthae <ahthaeon@pm.me>
2022-12-20 02:39:48 -0800

    Remove connection string from FusionContext

--- Data/Fusion/FusionModel.cs
+++ Data/Fusion/FusionModel.cs
@@ -105,8 +105,6 @@ namespace esquire.Data.Fusion
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseOracle("User Id=SYSTEM;Password=<password>;Data Source=localhost/XEPDB1");
             }
         }
 
