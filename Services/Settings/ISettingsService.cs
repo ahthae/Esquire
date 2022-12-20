aadc9e578e99b2e271612860bb0dab1da2b7eb50
ahthae <ahthaeon@pm.me>
2022-12-19 18:40:42 -0800

    Check config path during ServiceSettings initialization and add ServiceSettings write utility

--- Services/Settings/ISettingsService.cs
+++ Services/Settings/ISettingsService.cs
@@ -5,19 +5,20 @@ namespace esquire.Services.Settings;
 public interface ISettingsService
 {
     public Options Settings { get; set; }
+    public void Write();
 }
 
 public partial class Options : ObservableObject
 {
-    [ObservableProperty] private DatabaseOptions _database;
-    [ObservableProperty] private LoggingOptions _logging;
+    [ObservableProperty] private DatabaseOptions _database = new();
+    [ObservableProperty] private LoggingOptions _logging = new();
     
     public partial class DatabaseOptions : ObservableObject
     {
-        [ObservableProperty] private ProviderType _provider ;
-        [ObservableProperty] private string _userId ;
-        [ObservableProperty] private string _password ;
-        [ObservableProperty] private string _dataSource ;
+        [ObservableProperty] private ProviderType _provider = ProviderType.Oracle;
+        [ObservableProperty] private string _userId = "SYSTEM";
+        [ObservableProperty] private string _password = "";
+        [ObservableProperty] private string _dataSource  = "XEPDB1";
         
         public enum ProviderType
         {
@@ -27,13 +28,13 @@ public partial class Options : ObservableObject
 
     public partial class LoggingOptions : ObservableObject
     {
-        [ObservableProperty] private LogLevelOptions _logLevel;
+        [ObservableProperty] private LogLevelOptions _logLevel = new();
 
         public partial class LogLevelOptions : ObservableObject
         {
-            [ObservableProperty] private string _default;
-            [ObservableProperty] private string _system;
-            [ObservableProperty] private string _microsoft;
+            [ObservableProperty] private string _default = "Debug";
+            [ObservableProperty] private string _system = "Information";
+            [ObservableProperty] private string _microsoft = "Information";
         }
     }
 }
--- Services/Settings/SettingsService.cs
+++ Services/Settings/SettingsService.cs
@@ -1,4 +1,6 @@
+using System;
 using System.IO;
+using System.Text.Json;
 using CommunityToolkit.Mvvm.ComponentModel;
 using Microsoft.Extensions.Configuration;
 
@@ -8,14 +10,21 @@ public class SettingsService : ObservableObject, ISettingsService
 {
     private const string ConfigDirectory = "Config";
     private const string ConfigurationFile = "appsettings.json";
-    private Options _settings;
+    private Options _settings = new();
 
     public SettingsService()
     {
+        CheckAndCreateConfigPath();
         IConfiguration config = new ConfigurationBuilder()
-            .AddJsonFile(GetConfigPath())
+            .AddJsonFile(GetConfigPath(), false, true)
             .Build();
         Settings = config.Get<Options>();
+    }
+
+    public void Write()
+    {
+        CheckAndCreateConfigPath();
+        File.WriteAllText(GetConfigPath(), JsonSerializer.Serialize(Settings));
     }
     
     public Options Settings 
@@ -24,6 +33,22 @@ public class SettingsService : ObservableObject, ISettingsService
         set => SetProperty(ref _settings, value); 
     }
 
+    // Checks whether the config file path exists and creates it if it doesn't
+    private void CheckAndCreateConfigPath()
+    {
+        string configPath = GetConfigPath();
+        if (!(new DirectoryInfo(ConfigDirectory).Exists))
+        {
+            Console.WriteLine($"Creating new Config directory at {Path.GetFullPath(configPath)}");
+            Directory.CreateDirectory(ConfigDirectory);
+        }
+        if (!(new FileInfo(configPath).Exists))
+        {
+            Console.WriteLine($"Creating new config file at {Path.GetFullPath(configPath)}");
+            Write();
+        }
+    }
+    
     private static string GetConfigPath()
     {
         return Path.Join(ConfigDirectory, ConfigurationFile);
