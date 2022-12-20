91a7ef938d51c88709bede7227a7ded7a2c62639
ahthae <ahthaeon@pm.me>
2022-12-20 02:21:58 -0800

    Implement Sign-on connection test

--- ViewModels/SignOnViewModel.cs
+++ ViewModels/SignOnViewModel.cs
@@ -1,14 +1,20 @@
+using System;
+using System.Data.Common;
+using esquire.Services;
 using esquire.Services.Settings;
 
 namespace esquire.ViewModels;
 
 public class SignOnViewModel : ViewModelBase
 {
-    private ISettingsService _settingsService;
+    private readonly ISettingsService _settingsService;
+    private readonly IDatabaseService _databaseService;
     private Options _settings;
-    
-    public SignOnViewModel(ISettingsService settingsService)
+    private string _connectionTestResult;
+
+    public SignOnViewModel(ISettingsService settingsService, IDatabaseService databaseService)
     {
+        _databaseService = databaseService;
         _settingsService = settingsService;
         _settings = settingsService.Settings;
     }
@@ -19,7 +25,32 @@ public class SignOnViewModel : ViewModelBase
         set => SetProperty(ref _settings, value);
     }
 
-    public void ConfirmButton_OnClick()
+    public string ConnectionTestResult
+    {
+        get => _connectionTestResult;
+        set => SetProperty(ref _connectionTestResult, value);
+    }
+
+    public bool TestConnection()
+    {
+        try
+        {
+            DbConnection connection = _databaseService.GetConnection();
+            connection.Open();
+            connection.Close();
+            ConnectionTestResult = "Connection test successful!";
+            Console.WriteLine(ConnectionTestResult);
+            return true;
+        }
+        catch (Exception ex)
+        {
+            ConnectionTestResult = $"Connection test failed:\n{ex.Message}";
+            Console.WriteLine(ConnectionTestResult);
+            return false;
+        }
+    }
+    
+    public void SaveSettings()
     {
         _settingsService.Settings = Settings;
         _settingsService.Write();
